1c7c41f697e32c06e860ac0ab393f604992448c1
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-02-07 11:11:45 +0700

    Added data to list + broadcast reciever for time

--- app/src/main/java/com/example/ardin/forgetmenot/MainActivity.kt
+++ app/src/main/java/com/example/ardin/forgetmenot/MainActivity.kt
@@ -1,17 +1,51 @@
 package com.example.ardin.forgetmenot
 
+import android.annotation.TargetApi
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val taskList: MutableList<String> = mutableListOf()
     private val adapter by lazy { makeAdapter(taskList) }
     private val ADD_TASK_REQUEST = 1
+
+    private val tickReceiver by lazy { makeBroadcastReceiver() }
+
+    //that sets the date and time on the screen if it receives a time change broadcast from the system.
+    private fun makeBroadcastReceiver(): BroadcastReceiver {
+        return object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (intent?.action == Intent.ACTION_TIME_TICK) {
+                    dateTimeTextView.text = getCurrentTimestamps()
+                }
+            }
+
+        }
+    }
+
+    companion object {
+        private const val LOG_TAG = "MainActivityLog"
+
+        @TargetApi(Build.VERSION_CODES.N)
+        private fun getCurrentTimestamps(): String {
+            val simpleDate = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US)
+            val now = Date()
+            return simpleDate.format(now)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +57,23 @@ class MainActivity : AppCompatActivity() {
         taskListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id -> }
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        dateTimeTextView.text = getCurrentTimestamps()
+
+        registerReceiver(tickReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        try {
+            unregisterReceiver(tickReceiver)
+        } catch (e: IllegalArgumentException) {
+            Log.e(MainActivity.LOG_TAG, "time tick reciever not registered", e)
+        }
+    }
 
     fun addTaskClicked(view: View) {
         val intent = Intent(this, TaskDescriptionActivity::class.java)
@@ -31,4 +82,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeAdapter(list: List<String>): ArrayAdapter<String> =
             ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        //cek if activity
+        if (requestCode == ADD_TASK_REQUEST) {
+            //cek if activity is success
+            if (resultCode == Activity.RESULT_OK) {
+                //extract informasi dari intent
+                val task = data?.getStringExtra(TaskDescriptionActivity.EXTRA_TASK_DESCRIPTION)
+                task?.let {
+                    taskList.add(task)
+                    //trigger to refresh the view
+                    adapter.notifyDataSetChanged()
+                }
+
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/example/ardin/forgetmenot/TaskDescriptionActivity.kt
+++ app/src/main/java/com/example/ardin/forgetmenot/TaskDescriptionActivity.kt
@@ -1,8 +1,11 @@
 package com.example.ardin.forgetmenot
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_task_description.*
 
 class TaskDescriptionActivity : AppCompatActivity() {
     companion object {
@@ -14,5 +17,20 @@ class TaskDescriptionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_task_description)
     }
 
-    fun doneClicked(view: View) {}
+    fun doneClicked(view: View) {
+        //get value from descriptionText EditText
+        val taskDescription = descriptionText.text.toString()
+
+        //jika terdapat nilai yang kosong, maka kembali lagi ke main activity
+        if (taskDescription.isEmpty()) {
+            setResult(Activity.RESULT_CANCELED)
+        } else {
+            val result = Intent()
+            result.putExtra(EXTRA_TASK_DESCRIPTION, taskDescription)
+            setResult(Activity.RESULT_OK, result)
+        }
+
+        //close activity
+        finish()
+    }
 }
